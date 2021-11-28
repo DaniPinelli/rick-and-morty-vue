e1a305a3971cd5128267f675cc46438fbad46a6b
Daniel Pinelli <63522414+DaniPinelli@users.noreply.github.com>
2021-11-28 13:28:29 -0800

    First steps

--- src/App.vue
+++ src/App.vue
@@ -2,15 +2,35 @@
   <div id="app">
     <h1 class="title">Check Data</h1>
     <button v-on:click="fetch">Check</button>
+
+    <div v-for="character of characters" v-bind:key="character.id">
+      {{ character }}
+    </div>
   </div>
 </template>
 
 <script>
+//libraries
+import axios from "axios";
+
 export default {
   name: "App",
+  data: function () {
+    return {
+      characters: [],
+    };
+  },
   methods: {
     fetch() {
-      console.log("Hello World");
+      let result = axios
+        .get(" https://rickandmortyapi.com/api/character")
+        .then((res) => {
+          this.characters = res.data.results;
+          console.log(res.data);
+        })
+        .catch((err) => {
+          console.log(err);
+        });
     },
   },
 };
