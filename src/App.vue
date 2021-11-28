10dd8e8fdb79ebb7d29be256dbdd7c97f62f75ea
Daniel Pinelli <63522414+DaniPinelli@users.noreply.github.com>
2021-11-28 12:51:11 -0800

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    <h1 class="title">Check Data</h1>
+    <button v-on:click="fetch">Check</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App",
+  methods: {
+    fetch() {
+      console.log("Hello World");
+    },
+  },
+};
+</script>
+
+<style>
+</style>
